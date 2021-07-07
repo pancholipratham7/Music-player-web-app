a79dee02037ede22bff205bdcf096884f4ab84ba
Pratham Pancholi <pancholipratham33@gmail.com>
2021-07-07 12:32:27 +0530

    updating progressbar width with mouse click

--- js/script.js
+++ js/script.js
@@ -8,10 +8,14 @@ const playPause = document.querySelector(".play-pause-button");
 const playPauseIcon = document.querySelector(".play-pause-icon");
 const prevBtn = document.querySelector("#prev");
 const nextBtn = document.querySelector("#next");
+const songTotalDuration = document.querySelector(".end-time");
+const songCurrentDuration = document.querySelector(".start-time");
+const progressBar = document.querySelector(".progress-bar");
+const progressArea = document.querySelector(".progress-area");
 
 //State object will store the state of the application
 const state = {
-  musicIndex: 6,
+  musicIndex: 5,
   songState: "pause",
 };
 
@@ -21,6 +25,7 @@ const loadMusic = function (indexNumber) {
   musicName.textContent = allMusic[indexNumber - 1].name;
   musicArtist.textContent = allMusic[indexNumber - 1].artist;
   mainAudio.src = `songs/${allMusic[indexNumber - 1].src}.mp3`;
+  songTotalDuration.textContent = allMusic[indexNumber - 1].totalDuration;
 };
 
 //Loading the app
@@ -30,7 +35,6 @@ window.addEventListener("load", () => {
 
 //Function for pausing and playing the music....!
 const playAndPause = function () {
-  console.log("Hello");
   if (state.songState === "pause") {
     playPauseIcon.textContent = "pause";
     state.songState = "playing";
@@ -47,7 +51,7 @@ playPause.addEventListener("click", playAndPause);
 
 //Function for playing next music
 const nextMusic = function () {
-  state.musicIndex === 6
+  state.musicIndex === allMusic.length
     ? (state.musicIndex = 1)
     : (state.musicIndex = state.musicIndex + 1);
   state.songState = "pause";
@@ -58,7 +62,7 @@ const nextMusic = function () {
 //Function for playing previous music
 const prevMusic = function () {
   state.musicIndex === 1
-    ? (state.musicIndex = 6)
+    ? (state.musicIndex = allMusic.length)
     : (state.musicIndex = state.musicIndex - 1);
   state.songState = "pause";
   loadMusic(state.musicIndex);
@@ -68,3 +72,32 @@ const prevMusic = function () {
 //  Adding Event to play next and prev button
 nextBtn.addEventListener("click", nextMusic);
 prevBtn.addEventListener("click", prevMusic);
+
+//updating currenttime of the song
+mainAudio.addEventListener("timeupdate", function (e) {
+  let mins = Math.floor(mainAudio.currentTime / 60);
+  let secs = Math.floor(mainAudio.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+  const currentTime = e.target.currentTime;
+  const duration = e.target.duration;
+  progressBar.style.width = `${(currentTime / duration) * 100}%`;
+  songCurrentDuration.textContent = `${mins}:${secs}`;
+});
+
+progressArea.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("progress-area") ||
+    e.target.classList.contains("progress-bar")
+  ) {
+    const positionInfo = progressArea.getBoundingClientRect();
+    const width = Math.floor(positionInfo.width);
+    const percentSongPlayed = (e.offsetX / width) * 100;
+    progressBar.style.width = `${(e.offsetX / width) * 100}%`;
+    const calCurrentTime = (mainAudio.duration * percentSongPlayed) / 100;
+    mainAudio.currentTime = calCurrentTime;
+    state.songState = "pause";
+    playAndPause();
+  }
+});
